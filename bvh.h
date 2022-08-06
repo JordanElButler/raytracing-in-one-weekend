65dcaf6328ac610820279ff775f8912d963db1c7
blah <blah@blah.org>
2022-08-06 13:59:14 -0700

    aabb bug

--- bvh.h
+++ bvh.h
@@ -101,7 +101,9 @@ bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) co
         return false;
     
     bool hit_left = left->hit(r, t_min, t_max, rec);
-    bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
+//    bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
+    bool hit_right = right->hit(r, t_min, t_max, rec);
+
 
     return hit_left || hit_right;
 }
--- main.cpp
+++ main.cpp
@@ -244,6 +244,60 @@ hittable_list final_scene() {
 
     return objects;
 }
+
+hittable_list random_scene_bvh_test() {
+    hittable_list world;
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
+
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    auto center2 = center + vec3(0, random_double(0, 0.5), 0);
+                    
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0,1,0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4,1,0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4,1,0), 1.0, material3));
+
+    auto world_hierarchy = make_shared<bvh_node>(world, 0, 0);
+
+    hittable_list blah = hittable_list();
+    blah.add(world_hierarchy);
+
+    return blah;
+}
 int main() {
     auto aspect_ratio = 3.0 / 2.0;
     int image_width = 400;
@@ -319,19 +373,26 @@ int main() {
             lookat = point3(278, 278, 0);
             vfov = 40.0;
             break;
-
-        default:
         case 9:
             world = final_scene();
             aspect_ratio = 1.0;
-            image_width = 200;
-            samples_per_pixel = 200;
+            image_width = 300;
+            samples_per_pixel = 100;
             background = color(0,0,0);
             lookfrom = point3(478, 278, -600);
             lookat = point3(278, 278, 0);
             vfov = 40;
             break;
+        default:
 
+        case 10:
+            world = random_scene_bvh_test();
+            background = color(0.7, 0.8, 1.0);
+            lookfrom = point3(13,2,3);
+            lookat = point3(0, 0, 0);
+            vfov = 20.0;
+            aperture = 0.1;
+            break;
     }
 
     int image_height = static_cast<int>(image_width / aspect_ratio);
